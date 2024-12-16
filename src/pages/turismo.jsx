c1b8a02ccca3b683728e8c66a21eed4fd59aa349
Hernan Buzzi <138125329+hbuzzi74@users.noreply.github.com>
2024-12-16 17:42:52 -0300

    Se agregan las carpetas public y src

---
+++ src/pages/turismo.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Destinos from "../components/Destinos";
+
+const turismo = () => {
+  const [nombreQuery, setNombreQuery] = useState("");
+  const [categoriaQuery, setCategoriaQuery] = useState("");
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setNombreQuery(event.target[0].value);
+    setCategoriaQuery(event.target[1].value);
+  };
+
+  return (
+    <div>
+      <br></br>
+      <h1 className="text-5xl flex justify-center font-['Open_Sans']">
+        General La Madrid
+      </h1>
+      <h2 className="text-2xl flex justify-center italic">
+        Un oasis de tranquilidad en la Provincia de Buenos Aires{" "}
+      </h2>
+      <br></br>
+
+      <form action="" onSubmit={handleSearch}>
+        <div className="flex justify-center">
+          <input
+            type="text"
+            name="nombre"
+            placeholder="lugares.."
+            className="bg-gray-200 text-gray-800 px-2 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-md transition-all duration-300 w-2/5"
+          />
+          <select
+            type="select"
+            name="categoria"
+            // className="mr-4 bg-gray-200 relative w-[100px] h-[40px] justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
+            className="mr-4 bg-gray-200 relative w-2/5 h-[40px] justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
+          >
+            <option value="">Todas</option>
+            <option value="hospedaje">Hospedaje</option>
+            <option value="paseos">Paseos</option>
+          </select>
+        </div>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="mt-4 bg-green-500 p-1 w-[110px] font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white "
+          >
+            {" "}
+            Buscar{" "}
+          </button>
+        </div>
+        <br></br>
+      </form>
+      <Destinos nombre={nombreQuery} categoria={categoriaQuery} />
+    </div>
+  );
+};
+
+export default turismo;
