b605e21260c4f7be859dd164850e7e6a94a6e80d
hbuzzi74 <hbuzzi@gmail.com>
2024-12-18 17:09:34 -0300

    Se agregó el spinner mientras cargan los destinos

--- src/components/Destinos.jsx
+++ src/components/Destinos.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import "../estilos/imagen_spinner.css"; // Centrado de la imagen del spinner
 
 // import json_destinos from "../datos/destinos.json";
 // const URL_SERVICIO_DESTINOS = "http://localhost:3008/destinos";
 
 const Destinos = ({ nombre, categoria }) => {
   const [destinos, setDestinos] = useState([]);
+  const [loading, setLoading] = useState(true); // Estado del spinner (mostrar / ocultar)
 
   if (typeof nombre === "undefined") {
     nombre = ""; // Si no tenemos un nombre válido, lo convertimos a cadena vacía
@@ -24,73 +26,89 @@ const Destinos = ({ nombre, categoria }) => {
       })
       .catch((error) => {
         console.error("Error al obtener destinos: " + error);
-      });
+      })
+      .finally(() => setLoading(false));
   }, []);
 
   let destinosEncontrados = 0;
   return (
-    <div>
-      {destinos.map((destino) => {
-        if (
-          (nombre === "" && categoria === "") ||
-          (nombre === "" && destino.categoria.toLowerCase() === categoria) ||
-          (destino.nombre.toLowerCase().indexOf(nombre.toLowerCase()) != -1 &&
-            categoria === "") ||
-          (destino.nombre.toLowerCase().indexOf(nombre.toLowerCase()) != -1 &&
-            destino.categoria.toLowerCase() === categoria.toLowerCase())
-        ) {
-          destinosEncontrados++;
-          return (
-            <>
-              <div key={destino.id} id={destino.id}>
-                <h1 id="nombre">
-                  Nombre: <b>{destino.nombre}</b>
-                </h1>
-                <h2 id="descripcion">Descripción: {destino.descripcion}</h2>
-                <h3 id="categoria">
-                  Categoría: <b>{destino.categoria}</b>
-                </h3>
-                <p id="referencia">Ubicación: {destino.referencia}</p>
-                <p id="direccion">{destino.direccion}</p>
-                <a
-                  id="enlace_google_maps"
-                  href={destino.enlace_google_maps}
-                  target="_blank"
-                  style={{ color: "blue", textDecoration: "underline" }}
-                >
-                  Ver en Google Maps
-                </a>
-                {destino.imagenes.map((imagen) => {
-                  return (
-                    <img
-                      key={imagen}
-                      src={imagen}
-                      alt="Imagen de referencia"
-                    ></img>
-                  );
-                })}
-                <p id="horarios">Horario: {destino.horarios}</p>
-                <a
-                  id="url"
-                  href={destino.url}
-                  target="_blank"
-                  style={{ color: "blue", textDecoration: "underline" }}
-                >
-                  Sitio Web
-                </a>
-                <br />
-                <br />
-                <hr />
-                <br />
-              </div>
-            </>
-          );
-        }
-      })}
-      Total destinos encontrados: {destinosEncontrados}
-      <br></br>
-      <br></br>
-    </div>
+    <>
+      {loading ? (
+        <div className="imagen-spinner">
+          <img
+            src="/public/images/loading.gif"
+            alt="Cargando..."
+            style={{ alignself: "center" }}
+          />
+        </div>
+      ) : (
+        <div>
+          {destinos.map((destino) => {
+            if (
+              (nombre === "" && categoria === "") ||
+              (nombre === "" &&
+                destino.categoria.toLowerCase() === categoria) ||
+              (destino.nombre.toLowerCase().indexOf(nombre.toLowerCase()) !=
+                -1 &&
+                categoria === "") ||
+              (destino.nombre.toLowerCase().indexOf(nombre.toLowerCase()) !=
+                -1 &&
+                destino.categoria.toLowerCase() === categoria.toLowerCase())
+            ) {
+              destinosEncontrados++;
+              return (
+                <>
+                  <div key={destino.id} id={destino.id}>
+                    <h1 id="nombre">
+                      Nombre: <b>{destino.nombre}</b>
+                    </h1>
+                    <h2 id="descripcion">Descripción: {destino.descripcion}</h2>
+                    <h3 id="categoria">
+                      Categoría: <b>{destino.categoria}</b>
+                    </h3>
+                    <p id="referencia">Ubicación: {destino.referencia}</p>
+                    <p id="direccion">{destino.direccion}</p>
+                    <a
+                      id="enlace_google_maps"
+                      href={destino.enlace_google_maps}
+                      target="_blank"
+                      style={{ color: "blue", textDecoration: "underline" }}
+                    >
+                      Ver en Google Maps
+                    </a>
+                    {destino.imagenes.map((imagen) => {
+                      return (
+                        <img
+                          key={imagen}
+                          src={imagen}
+                          alt="Imagen de referencia"
+                        ></img>
+                      );
+                    })}
+                    <p id="horarios">Horario: {destino.horarios}</p>
+                    <a
+                      id="url"
+                      href={destino.url}
+                      target="_blank"
+                      style={{ color: "blue", textDecoration: "underline" }}
+                    >
+                      Sitio Web
+                    </a>
+                    <br />
+                    <br />
+                    <hr />
+                    <br />
+                  </div>
+                </>
+              );
+            }
+          })}
+          Total destinos encontrados: {destinosEncontrados}
+          <br></br>
+          <br></br>
+        </div>
+      )}
+    </>
   );
 };
 
